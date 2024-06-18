132bc9700bd8b54186f80d4f985f446e5772257a
webmaster-studio <airyland@becool.me>
2024-06-18 10:35:21 +0800

    remove google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css"
 import { Metadata, type Viewport } from "next"
-import { GoogleAnalytics } from "@next/third-parties/google"
+// import { GoogleAnalytics } from "@next/third-parties/google"
 
 import { siteConfig } from "@/config/site"
 import { fontMuseo, fontSans } from "@/lib/fonts"
@@ -77,7 +77,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <TailwindIndicator />
           </ThemeProvider>
         </body>
-        <GoogleAnalytics gaId="G-20G8R0K6W9" />
+        <!--<GoogleAnalytics gaId="G-20G8R0K6W9" />-->
       </html>
     </>
   )
